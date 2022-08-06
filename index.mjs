6eec5c6228c7196a90040a15fe4981d4ae556f19
erobert <etiennerobert33@gmail.com>
2022-08-06 23:12:19 +0200

    Create convert function

--- index.mjs
+++ index.mjs
@@ -1,6 +1,19 @@
 import fs from "fs";
 import { parse } from "csv-parse";
 
+function convert(data) {
+  const isIncome = data["Transaction type"] === "Income";
+  const amount = data["Amount (EUR)"];
+  const flow = isIncome
+    ? { Inflow: amount }
+    : { Outflow: (-parseFloat(amount)).toString() };
+  return {
+    Date: data.Date,
+    Payee: data.Payee,
+    ...flow,
+  };
+}
+
 fs.createReadStream("./example.csv")
   .pipe(parse({ delimiter: ",", columns: true }))
-  .on("data", (data) => console.log(data));
+  .on("data", (data) => console.log(convert(data)));
