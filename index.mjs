9d98a9ee48bd3cd4f2bea78f197f81a754df457f
erobert <etiennerobert33@gmail.com>
2022-08-07 13:55:05 +0200

    Use command line args

--- index.mjs
+++ index.mjs
@@ -3,6 +3,19 @@ import { parse } from "csv-parse";
 import { transform } from "stream-transform";
 import { stringify } from "csv-stringify";
 
+function usage() {
+  const node = process.argv[0];
+  const file = process.argv[1];
+  console.log(`usage: ${node} ${file} [input.csv]`);
+}
+
+const myArgs = process.argv.slice(2);
+if (myArgs.length < 1) {
+  usage();
+  process.exit();
+}
+const [inputFile] = myArgs;
+
 function convert(data) {
   const isIncome = data["Transaction type"] === "Income";
   const amount = data["Amount (EUR)"];
@@ -16,7 +29,7 @@ function convert(data) {
   };
 }
 
-fs.createReadStream("./example.csv")
+fs.createReadStream(inputFile)
   .pipe(parse({ delimiter: ",", columns: true }))
   .pipe(transform(convert))
   .pipe(
